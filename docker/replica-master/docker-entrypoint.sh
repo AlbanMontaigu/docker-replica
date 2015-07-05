667e7188ace69ee47816894920e267f7b2ccac41
Alban Montaigu <alban.montaigu@gmail.com>
2015-07-06 00:11:05 +0200

    Logs added to master

--- docker/replica-master/docker-entrypoint.sh
+++ docker/replica-master/docker-entrypoint.sh
@@ -6,12 +6,16 @@ trap 'kill -TERM $PID; wait $PID; exit 0' TERM INT
 # Want to have sync for ever...
 while true; do
 
+	# Log message for syncing start
+	echo "[$(date)] Syncing start"
+
 	# Now running sync !
 	sshpass -p $REPLICA_SLAVE_PWD unison -batch -confirmbigdel=false $REPLICA_DATA_DIR ssh://replica-slave@$REPLICA_SLAVE_HOST/$REPLICA_DATA_DIR &
 	PID=$!
 	wait $PID
 
 	# Not sync every second !
+	echo "[$(date)] Syncing end, now sleeping for $REPLICA_INTERVAL seconds"
 	sleep $REPLICA_INTERVAL &
 	PID=$!
 	wait $PID
