6dbe692cdde22bd737e0443fe3177bf1353696ba
Alban Montaigu <alban.montaigu@gmail.com>
2015-07-23 00:13:34 +0200

    Try to solve permission denied => switch to root

--- docker/replica-slave/docker-entrypoint.sh
+++ docker/replica-slave/docker-entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 # If no configuration done, do it
 if env | grep -q ^REPLICA-SLAVE-PWD=; then
 
-	# Changing password with the specified one to not use default password
-	echo 'replica-slave:$REPLICA-SLAVE-PWD' | chpasswd
+    # Changing password with the specified one to not use default password
+    echo 'root:$REPLICA-SLAVE-PWD' | chpasswd
 
 fi
 
