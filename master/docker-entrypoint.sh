0965a84d527fc124bac0abea5742ed1252953a60
Alban Montaigu <alban.montaigu@gmail.com>
2015-07-26 23:41:50 +0200

    Better configuration + permanent & watch system

--- master/docker-entrypoint.sh
+++ master/docker-entrypoint.sh
@@ -1,23 +1,70 @@
 #!/bin/sh
 
-# Want to exit cleanly with subprocess
-trap 'kill -TERM $PID; wait $PID; exit 0' TERM INT
+# Configuration is in docker file
+UNISON_PRF_PATH="${UNISON_DIR}/${UNISON_PRF_FILE}"
 
-# Want to have sync for ever...
-while true; do
+#
+# Generate unison dir if necessary 
+#
+if [ -d "${UNISON_DIR}" ]; then
+    sudo mkdir -p --mode=777 "${UNISON_DIR}"
+fi
 
-    # Log message for syncing start
-    echo "[$(date)] Syncing start"
+#
+# Generate unison configuration if necessary
+#
+# /!\ Be carefull, if you change any config value, this file must be deleted or replaced /!\
+#
+if [ -e "${UNISON_PRF_PATH}" ]; then
 
-    # Now running sync !
-    sshpass -p $REPLICA_SLAVE_PWD unison -batch -confirmbigdel=false $REPLICA_DATA_DIR ssh://root@$REPLICA_SLAVE_HOST/$REPLICA_DATA_DIR -sshargs "-p ${REPLICA_SLAVE_PORT}" &
-    PID=$!
-    wait $PID
+echo -e "
+# Batch mode
+batch = true
+auto = true
 
-    # Not sync every second !
-    echo "[$(date)] Syncing end, now sleeping for $REPLICA_INTERVAL seconds"
-    sleep $REPLICA_INTERVAL &
-    PID=$!
-    wait $PID
+# No backup in addition to sync...
+backups = false
 
-done
+# Source folder to sync
+root = ${REPLICA_DATA_DIR}
+
+# Destination (and distant) folder to sync
+root = ssh://root@${REPLICA_SLAVE_HOST}/${REPLICA_DATA_DIR}
+sshargs = -p ${REPLICA_SLAVE_PORT}
+
+# Logs but not too much...
+log = true
+logfile = ${UNISON_DIR}/unison.log
+silent = true
+
+# Sync date and keep recent files
+times = true
+force = newer
+
+# Want to keep owner and group
+owner = true
+group = true
+
+# Do not want cofirm in batch mode
+confirmmerge = false
+confirmbigdel = false
+
+# Want to manage conflic in a safe way
+copyonconflict = true
+prefer = newer
+
+# Want to retry if failure
+maxerrors = 10
+retry = 10
+
+# Want to sync when a file change (normally with python-pyinotify)
+repeat = watch
+
+" > $UNISON_PRF_PATH
+
+fi
+
+#
+# Now starting sync in endless mode thanks to repeat = watch
+#
+exec sshpass -p $REPLICA_SLAVE_PWD unison "${UNISON_PRF}"
